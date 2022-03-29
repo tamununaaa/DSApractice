50b64abe1b0c52b5fb6422d727173d246f735375
Tamanna <tamannakapoor144@gmail.com>
2022-03-29 12:56:20 +0530

    added bubble sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -17,7 +17,7 @@ public class bubbleSort {
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
-        
+         
         for(int i=0;i<n;i++)
                 System.out.print(arr[i]+ " ");  //print sorted array
 
