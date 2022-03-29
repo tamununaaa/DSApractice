f3e8358c91fc0b2e9f8c7e3d548a0a4bdbbbc5a2
Tamanna <tamannakapoor144@gmail.com>
2022-03-29 13:08:24 +0530

    insertion sort

---
+++ selectionSort.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class selectionSort {
+    public static void main(String args[]
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i=0;i<n-1;i++)
+
+    }
+    
+}
