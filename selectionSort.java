ae57b6993128a70da800145470346db0e49e76c6
Tamanna <tamannakapoor144@gmail.com>
2022-03-29 13:16:22 +0530

    added selectio sort

---
+++ selectionSort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class selectionSort {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i=0;i<n;i++)
+            arr[i] = sc.nextInt();
+        
+        for(int i=0;i<n-1;i++)
+        {
+            int mindex = i;
+
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[j]<arr[mindex])
+                    mindex = j;
+
+                int temp = arr[i];
+                arr[i] = arr[mindex];
+                arr[mindex] = temp;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+            System.out.print(arr[i] + " ");
+    }
+    
+}
