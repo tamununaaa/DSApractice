1bb6aff650e5545e39162a48757f8b58e6a8cf06
Tamanna Kapoor <89573580+tamununaaa@users.noreply.github.com>
2022-05-22 12:06:24 +0530

    Add files via upload

---
+++ Stacks/General Stack Operations/emptyMethod.java
@@ -0,0 +1,25 @@
+/*
+    The empty() method returns a boolean value.
+    1. If the stack is empty, it returns TRUE.
+    2. If the stack is not empty, it returns FALSE.
+*/
+
+package Stacks;
+import java.util.Stack;
+import java.util.*;
+public class emptyMethod {
+    public static void main(String args[])
+    {
+        Stack<Integer> stack = new Stack<Integer>();                   //Create stack of Integer type
+
+        boolean res = stack.empty();                            //res is a boolean indicating whether stack is empty or not
+        System.out.println("The stack is empty? " + res);       //Print result- true or false
+        
+        stack.push(200);                                  //Push elements in the stack
+        stack.push(201);
+
+        res = stack.empty();                                   //res is a boolean indicating whether stack is empty or not
+        System.out.println("The stack is empty? " + res);      //Print result- true or false
+
+    }
+}
